8154e6030269ffd18b56991698395d00263c1ce0
sopheaknova <novadesign.dev@gmail.com>
2014-08-07 18:10:28 +0700

    Disable season

--- library/custom-posts/custom-posts.php
+++ library/custom-posts/custom-posts.php
@@ -12,7 +12,6 @@ load_template( SP_BASE_DIR . 'library/custom-posts/cp-tv.php' );
 load_template( SP_BASE_DIR . 'library/custom-posts/cp-radio.php' );
 
 //Taxonomies
-load_template( SP_BASE_DIR . 'library/custom-posts/taxonomy-season.php' );
 load_template( SP_BASE_DIR . 'library/custom-posts/taxonomy-tv.php' );
 load_template( SP_BASE_DIR . 'library/custom-posts/taxonomy-radio.php' );
 load_template( SP_BASE_DIR . 'library/custom-posts/taxonomy-episode.php' );
